9ec3fa9381f1f487f3e95eebee22f97cdb7124c9
LastBit97 <diman01.97@gmail.com>
2023-02-23 00:06:51 +0300

    Реализована программа
    Реализована программа, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равно 3 символам

--- StringArray/Program.cs
+++ StringArray/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string[] originalArray = { "hello", "2", "world", ":-)" };
+
+        int count = 0;
+        for (int i = 0; i < originalArray.Length; i++)
+        {
+            if (originalArray[i].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        string[] newArray = new string[count];
+        int index = 0;
+        for (int i = 0; i < originalArray.Length; i++)
+        {
+            if (originalArray[i].Length <= 3)
+            {
+                newArray[index] = originalArray[i];
+                index++;
+            }
+        }
+
+        Console.WriteLine("Исходный массив: " + string.Join(", ", originalArray));
+        Console.WriteLine("Новый массив: " + string.Join(", ", newArray));
+    }
+}
